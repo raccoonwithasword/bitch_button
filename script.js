d3853a463221ab7fd8cc2d81b052e1a9b051094c
raccoonwithasword <percy.ekanem@gmail.com>
2021-08-22 20:20:36 +0100

    Added text

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.querySelector("button");
 
 button.onclick = function () {
 let name = prompt ("What is your name?");
-    alert (`Hi, ${name}, you're a little bitch.`);
+    alert (`Hi, ${name}. Thank you for clicking. I would like to formally inform you that you're a little bitch.`);
 }
